21d8072f7e3aa4bb6ae483c579cade39c8134a13
EC2 Default User <ec2-user@ip-172-31-37-32.eu-west-2.compute.internal>
2021-03-03 19:50:48 +0000

    added asg

---
+++ basic_terraform4/asg.tf
@@ -0,0 +1,28 @@
+resource "aws_autoscaling_group" "bar" {
+name = "terraform-asg-yusuf"
+launch_configuration = "${aws_launch_configuration.as_conf.name}"
+availability_zones = data.aws_availability_zones.all.names
+min_size = 1
+max_size = 2
+lifecycle {
+create_before_destroy = true  # this is important. for example s3 bucket as well. 
+}
+}
+
+
+#resource "aws_autoscaling_group" "yusuf" {
+#name = "terraform-asg-yusuf"
+#launch_configuration = "${aws_launch_configuration.newlaunch.id}"
+#availability_zones = data.aws_availability_zones.all.names
+#min_size = 1
+#max_size = 10
+#tags {
+#    key = "name"
+#    value = "terraform-asg-yusuf"
+#    propagate_at_launch = true
+#}
+
+#"us-east-1a", or you can specify each az here or you can use data.aws_availability_zone.all.names 
+#"us-east-1b", or you can specify each az here or you can use data.aws_availability_zone.all.names 
+#"us-east-1c", or you can specify each az here or you can use data.aws_availability_zone.all.names 
+# but you have top specify under image.tf file
---
+++ basic_terraform4/image.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "image" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_availability_zones" "all" {} # this code pulls all az from regigion which is defined on provider.tf 
+
+#output "AZ" {
+#    value = data.aws_availability_zones.all.names 
+#}
