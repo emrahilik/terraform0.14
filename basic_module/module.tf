402ca0071175b5d6f214dc84b4cb456c964ed2e5
EC2 Default User <ec2-user@ip-172-31-37-32.eu-west-2.compute.internal>
2021-03-21 06:35:07 +0000

    add basic module integrated with basic_terraform4

--- basic_module/module.tf
+++ basic_module/module.tf
@@ -1,12 +1,26 @@
 module "wordpress"{
     source = "../basic_terraform4"  # or give the github relative path 
-}
+                        # if you want to use your module and deploy diffrent region you have to go your code 
+                        # and you have to work with your provider. 
+    aws_region = "us-east-1"
+    desired_capacity   = 1
+    max_size           = 1
+    min_size           = 1
 
+}
 
 module "joomla"{
     source = "../basic_terraform4"  # or give the github relative path 
+    aws_region = "us-east-1"
+    desired_capacity   = 1
+    max_size           = 1
+    min_size           = 1
 }
 
 module "apache"{
     source = "../basic_terraform4"  # or give the github relative path 
+    aws_region = "us-east-1"
+    desired_capacity   = 1
+    max_size           = 1
+    min_size           = 1
 }
